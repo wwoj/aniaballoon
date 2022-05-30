b5af7d4b60b75ffbca2fbf181b58861c4bcee24f
wwoj <wojcik.wojtek@yahoo.pl>
2022-05-30 17:42:46 +0200

    Step  #3

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import i18n from './Languages/i18n';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <App  i18n={i18n}/>
   </React.StrictMode>
 );
 
