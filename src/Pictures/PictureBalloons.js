a91cdf0a13df92fe9448541af9fd4049d493b4c9
wwoj <wojcik.wojtek@yahoo.pl>
2022-06-14 11:53:29 +0200

    Azure test 3 - ballons galery

--- src/Pictures/PictureBalloons.js
+++ src/Pictures/PictureBalloons.js
@@ -1,56 +1,64 @@
-import Ballon_1 from './Balloons/bal_1.png';
-import Ballon_2 from './Balloons/bal_2.png';
-import Ballon_3 from './Balloons/bal_3.png';
-import Ballon_4 from './Balloons/bal_4.png';
-import Ballon_5 from './Balloons/bal_5.png';
-import Ballon_6 from './Balloons/bal_6.png';
-import Ballon_7 from './Balloons/bal_7.png';
-import Ballon_8 from './Balloons/bal_8.png';
-import Ballon_9 from './Balloons/bal_9.png';
-import Ballon_10 from './Balloons/bal_10.png';
-import Ballon_11 from './Balloons/bal_11.png';
-import Ballon_12 from './Balloons/bal_12.png';
-import Ballon_13 from './Balloons/bal_13.png';
-import Ballon_14 from './Balloons/bal_14.png';
-import Ballon_15 from './Balloons/bal_15.png';
-import Ballon_16 from './Balloons/bal_16.png';
-import Ballon_17 from './Balloons/bal_17.png';
-import Ballon_18 from './Balloons/bal_18.png';
-import Ballon_19 from './Balloons/bal_19.png';
-import Ballon_20 from './Balloons/bal_20.png';
-import Ballon_21 from './Balloons/bal_21.png';
-import Ballon_22 from './Balloons/bal_22.png';
+import Balloon_1 from './Balloons/bal_1.png';
+import Balloon_2 from './Balloons/bal_2.png';
+import Balloon_3 from './Balloons/bal_3.png';
+import Balloon_4 from './Balloons/bal_4.png';
+import Balloon_5 from './Balloons/bal_5.png';
+import Balloon_6 from './Balloons/bal_7.png';
+import Balloon_7 from './Balloons/bal_7.png';
+import Balloon_8 from './Balloons/bal_8.png';
+import Balloon_9 from './Balloons/bal_9.png';
+import Balloon_10 from './Balloons/bal_10.png';
+import Balloon_11 from './Balloons/bal_11.png';
+import Balloon_12 from './Balloons/bal_12.png';
+import Balloon_13 from './Balloons/bal_13.png';
+import Balloon_14 from './Balloons/bal_14.png';
+import Balloon_15 from './Balloons/bal_15.png';
+import Balloon_16 from './Balloons/bal_16.png';
+import Balloon_17 from './Balloons/bal_17.png';
+import Balloon_18 from './Balloons/bal_18.png';
+import Balloon_19 from './Balloons/bal_19.png';
+import Balloon_20 from './Balloons/bal_20.png';
+import Balloon_21 from './Balloons/bal_21.png';
+import Balloon_22 from './Balloons/bal_22.png';
 
-const exportArray = [{obj:Ballon_1,title:"Balloon_1"},
-{obj:Ballon_2,title:"Balloon_2"},
-{obj:Ballon_3,title:"Balloon_3"},
-{obj:Ballon_4,title:"Balloon_4"},
-{obj:Ballon_5,title:"Balloon_5"},
-{obj:Ballon_6,title:"Balloon_6"},
-{obj:Ballon_7,title:"Balloon_7"},
-{obj:Ballon_8,title:"Balloon_8"},
-{obj:Ballon_9,title:"Balloon_9"},
-{obj:Ballon_10,title:"Balloon_10"},
-{obj:Ballon_11,title:"Balloon_11"},
-{obj:Ballon_12,title:"Balloon_12"},
-{obj:Ballon_13,title:"Balloon_13"},
-{obj:Ballon_14,title:"Balloon_14"},
-{obj:Ballon_15,title:"Balloon_15"},
-{obj:Ballon_16,title:"Balloon_16"},
-{obj:Ballon_17,title:"Balloon_17"},
-{obj:Ballon_18,title:"Balloon_18"},
-{obj:Ballon_19,title:"Balloon_19"},
-{obj:Ballon_20,title:"Balloon_20"},
-{obj:Ballon_21,title:"Balloon_21"},
-{obj:Ballon_22,title:"Balloon_22"}
-]
+
+
+
+
+
+const exportArray = [
+    {obj:Balloon_1,title:"Balloon_1"},
+    {obj:Balloon_2,title:"Balloon_2"},
+    {obj:Balloon_3,title:"Balloon_3"},
+    {obj:Balloon_4,title:"Balloon_4"},
+    {obj:Balloon_5,title:"Balloon_5"},
+    {obj:Balloon_6,title:"Balloon_6"},
+    {obj:Balloon_7,title:"Balloon_7"},
+    {obj:Balloon_8,title:"Balloon_8"},
+    {obj:Balloon_9,title:"Balloon_9"},
+    {obj:Balloon_10,title:"Balloon_10"},
+    {obj:Balloon_11,title:"Balloon_11"},
+    {obj:Balloon_12,title:"Balloon_12"},
+    {obj:Balloon_13,title:"Balloon_13"},
+    {obj:Balloon_14,title:"Balloon_14"},
+    {obj:Balloon_15,title:"Balloon_15"},
+    {obj:Balloon_16,title:"Balloon_16"},
+    {obj:Balloon_17,title:"Balloon_17"},
+    {obj:Balloon_18,title:"Balloon_18"},
+    {obj:Balloon_19,title:"Balloon_19"},
+    {obj:Balloon_20,title:"Balloon_20"},
+    {obj:Balloon_21,title:"Balloon_21"},
+    {obj:Balloon_22,title:"Balloon_22"}
+];
 export function GetPictures() {
-  const promise = new Promise((resolve, rejected) => {
-    setTimeout(() => {
-      resolve(exportArray);
-    }, 3000);
-  });
+    const promise = new Promise((resolve, rejected) => {
+      setTimeout(() => {
+        resolve(exportArray);
+      }, 3000);
+    });
+  
+    return promise;
+  }
+  
 
-  return promise;
-}
 export default exportArray;
